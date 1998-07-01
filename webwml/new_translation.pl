b39769d88359233abf18e7a9959be696d00942a7
James A. Treacy <treacy@debian.org>
1998-07-01 04:32:19 +0000

    Imported source

---
+++ webwml/new_translation.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+
+require 5.001;
+use strict;
+
+my (@languages, @parts, $file, $filename, $lang, $path, $pid);
+
+if (!@ARGV) {
+   print "Usage: new_translation.pl <file1> <file2>...\n";
+   print "\tThis will update every version of <file?> so that they\n";
+   print "\tknow about the new translation. Each <file?> should be\n";
+   print "\tthe path to a .wml file (without the language directory).\n";
+   exit 1;
+}
+
+opendir(DIR, ".") || die "can't open directory $!";
+@languages = grep { /^\w+$/ && -d $_ } readdir(DIR);
+closedir DIR;
+# print @languages;
+
+foreach $file (@ARGV) {
+   foreach $lang (@languages) {
+      @parts = split ?\/?,$file;
+      $filename = pop(@parts);
+      $path = join('/', @parts);
+      if ( -f "$lang/$file" ) {
+         print "running 'wml $lang/$path/$filename'\n";
+         $pid = fork;
+         if ($pid) { # parent
+            # do nothing
+         }
+         else {      # child
+            chdir "$lang/$path" && system("wml", $filename);
+            exit 0;
+         }
+         waitpid($pid,0);
+      }
+      else {
+         print "$lang/$file doesn't exist\n";
+      }
+   }
+}
