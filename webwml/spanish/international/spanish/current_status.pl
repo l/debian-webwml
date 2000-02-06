e01bafe0e79977b8cfd97026d9df8586202dc2ce
spanish <>
2000-02-06 08:57:22 +0000

    Broke ltcp.body in little files and fixed the database, now correctly makes the documents available.

--- webwml/spanish/international/spanish/current_status.pl
+++ webwml/spanish/international/spanish/current_status.pl
@@ -6,7 +6,8 @@ do 'documents-bd.pl';
 # all information regarding the translation status for a fixed 
 # language
 
-#$$translations{'key'} = {
+#%translations_status = (
+#  'key' => {
 #	'status'		=> '',
 #use numbers instead of text since it makes translations easier
 #               'not-available' - 0
@@ -44,7 +45,8 @@ do 'documents-bd.pl';
 #	'translation_package'	=> '',
 #	'last_translated'	=> '',
 #	'old_translators'	=> []
-#};
+#},
+#);
 
 
 # ########################################################################################
@@ -53,9 +55,11 @@ do 'documents-bd.pl';
 #
 # ########################################################################################
 
-$$translations{'book-gtiau'} = {
+%translations_status = (
+
+'book-gtiau' => {
 	'status' => 1
-};
+},
 
 
 # ########################################################################################
@@ -64,93 +68,92 @@ $$translations{'book-gtiau'} = {
 #
 # ########################################################################################
 
-$$translations{'project-history'} = {
+      'project-history' => {
 	'status'	=> 2,
 	'since'			=> '24/01/2000',
 	'translation_name'	=> 'Historia del proyecto Debian',
 	'translation_maintainer'=> ['Enrique Zanardi <ezanardi@id-agora.com>']
-};
+},
 
-$$translations{'user'} = {
+	'user' => {
 	'status'	=> 2,
 	'since'			=> '24/01/2000',
 	'translation_name'	=> 'Guía para el usuario de Debian',
 	'translation_maintainer'=> ['Enrique Zanardi <ezanardi@id-agora.com>']
-};
+},
 
-$$translations{'faq'} = {
+	'faq' => {
 	'status'		=> 4,
 	'since'			=> '?',
 	'translation_name'	=> 'FAQ de Debian GNU/Linux',
 	'translation_maintainer'=> ['Santiago Vila <sanvila@ctv.es>'],
 	'translation_package'	=> 'doc-debian-es'
-};
-
-#
+},
 
 
-$$translations{'tutorial'} = {
+	'tutorial' => {
 	'status'		=> 3,
 	'since'			=> '31/01/1999',
 	'translation_name'	=> 'Tutorial de Debian',
 	'translation_maintainer'=> ['Javier Fernández-Sanguino Peña <jfs@computer.org>'],
-	'translation_cvs_url' 	=>'http://www.debian.org/cgi-bin/cvsweb/manuals.sgml/debian-tutorial.es.sgml'
-};
+	'translation_cvs_url' 	=>'http://cvs.debian.org/ddp/manuals.sgml/debian-tutorial.es.sgml?cvsroot=debian-doc'
+},
 
 
 
-$$translations{'install'} = {
+	'install' => {
 	'status'		=> 4,
 	'translation_name'	=> 'Manual de instalación de Debian',
-	'translation_maintainer'=> ['Enrique Zanardi'],
+	'translation_maintainer'=> ['Enrique Zanardi <ezanardi@debian.org>'],
 	'base_revision'		=> '2.1',
-	'translation_cvs_url'   => 'http://www.debian.org/cgi-bin/cvsweb/debian-boot/boot-floppies/documentation/install.es.sgml',
+	'translation_cvs_url'   => 'http://cvs.debian.org/debian-boot/boot-floppies/documentation/install.es.sgml?cvsroot=debian-boot',
 	'translation_package'	=> 'boot-floppies',
 	'last_translated'	=> '16/03/1999'
-};
+},
 
-$$translations{'dselect-beginner'} = {
+	'dselect-beginner' => {
 	'status'		=> 4,
 #	'since'			=> '22/03/1999',
 	'translation_name'	=> 'Guía de dselect para principiantes',
-	'translation_maintainer'=> ['Enrique Zanardi'],
+	'translation_maintainer'=> ['Enrique Zanardi <ezanardi@debian.org>'],
 #	'base_revision'		=> '2.1',
 #	'translation_url'	=> 'http://www.debian.org/~clebars/docs-2.1/dselect-beginner.html/dselect-beginner.html',
-	'translation_cvs_url'   => 'http://www.debian.org/cgi-bin/cvsweb/debian-boot/boot-floppies/documentation/dselect-beginner.es.sgml',
+	'translation_cvs_url'   => 'http://cvs.debian.org/debian-boot/boot-floppies/documentation/dselect-beginner.es.sgml?cvsroot=debian-boot',
 	'translation_package'	=> 'boot-floppies'
 #	'last_translated'	=> '22/003/1999',
-};
+},
 
-$$translations{'release-notes'} = {
+	'release-notes' => {
 	'status'		=> 1,
 #	'since'			=> '05/06/1999',
 	'translation_name'	=> 'Notas de la versión',
-	'translation_maintainer'=> ['Enrique Zanardi'],
-	'base_revision'		=> '',
-	'translation_url'	=> '',
-	'translation_cvs_url'   => '',
+	'translation_maintainer'=> ['Enrique Zanardi <ezanardi@debian.org>'],
+#	'base_revision'		=> '',
+#	'translation_url'	=> '',
+#	'translation_cvs_url'   => '',
 	'translation_package'	=> 'boot-floppies',
-	'last_translated'	=> ''
-};
+#	'last_translated'	=> ''
+},
 
 
 
-$$translations{'maint-guide'} = {
+	'maint-guide' => {
 	'status'		=> 6,
 	'since'			=> '3/9/1999',
 	'translation_name'	=> 'Guía del Nuevo Desarrollador de Debian',
 	'translation_maintainer'=> ['Javier Fernández-Sanguino Peña <jfs@computer.org>'],
 	'translation_dev_url'	=> 'http://www.dat.etsit.upm.es/~jfs/debian/maint-guide-es/',
-	'translation_cvs_url'   => 'http://www.debian.org/cgi-bin/cvsweb/ddp/manuals.sgml/maint-guide/maint-guide.es.sgml',
+	'translation_cvs_url'   => 'http://cvs.debian.org/ddp/manuals.sgml/maint-guide/maint-guide.es.sgml?cvsroot=debian-doc',
 	'translation_package'   => 'maint-guide-es',
 	'last_translated'	=> '12/04/1999'
-};
+},
 
-$$translations{'programmer'} = {
+	'programmer' => {
 	'status'		=> 4,
 	'since'			=> '13/04/1998',
         'translation_name'      => 'Cómo hacer paquetes Debian (nuevos desarrolladores)',
         'translation_maintainer'=> ['Javier Fernández-Sanguino Peña <jfs@computer.org>'],
         'translation_dev_url'   => 'http://www.dat.etsit.upm.es/~jfs/debian/doc//'
-};
+},
 
+);
