0edd9597a35ee519ed471f0ea2631bbf8a759fdd
french <>
2000-12-21 07:51:11 +0000

    First version of the database of translators used in checktrans.pl

---
+++ webwml/french/international/french/translator.db.pl
@@ -0,0 +1,136 @@
+#!/usr/bin/perl
+
+# This is GPL'ed, copyright 2000 Martin Quinson <mquinson@ens-lyon.fr>
+
+# In this file, you can find a DB about the translators. 
+# It should be hand maintained by the coordinator, it is not modified
+#  automatically.
+# For now, it is only used by check-translation.pl (in a version not 
+#  commited to the cvs, but I can send it to you, if you want to)
+
+# Here is the syntax:
+#  The data is in a hash table named 'translators'. 
+#  Each key is the name of a translator (trimmed, without email adress)
+#  For each one, you have a (sub)hash table containing:
+#    email => the current email of this guy
+#    informations to describe what sort of automatic mail he/she want to get.
+#     For each part, the value is numeric and means:
+#        0: never, 1:monthly, 2:weekly, 3:daily
+#     The different parts are:
+#        summary: just the summary of what is needed
+#          example: NeedToUpdate french/donations.wml from version 1.16 to version 1.29
+#        logs: the cvs logs between the translated and actual version
+#        diff: idem with the diff
+#        file: add the last version of translated file (to avoid to cvs update it before work)
+#   compress: which type of compression you want to have
+#     it could be: "none","gzip" ou "bzip2" 
+#     (NOT IMPLEMENTED: for now, "none" is assumed in all case)
+
+sub init_translators {
+ $translators = {
+    'Denis Barbier' => {
+         'email'     => 'barbier@imacs.polytechnique.fr',
+	 'summary'   => 0,
+	 'logs'      => 0,
+         'diff'      => 0,
+         'file'      => 0,
+	 'compress'  => 'none'
+    },
+    'Antoine Martin' => {
+         'email'     => 'amartin@atos-group.com',
+	 'summary'   => 0,
+	 'logs'      => 0,
+         'diff'      => 0,
+         'file'      => 0,
+	 'compress'  => 'none'
+    },
+    'Sébastien Kalt' => {
+         'email'     => 'ustilago@bigfoot.com',
+         'summary'   => 0, 
+         'logs'      => 0, 
+         'diff'      => 0, 
+	 'file'      => 0,
+         'compress'  => 'none' 
+    },
+    'Jérôme Marant' => {
+         'email'     => 'jerome.marant@free.fr',
+         'summary'   => 0, 
+         'logs'      => 0, 
+         'diff'      => 0, 
+	 'file'      => 0,
+         'compress'  => 'none' 
+    },
+    'Christian Couder' => {
+         'email'     => 'chcouder@club-internet.fr',
+         'summary'   => 3, 
+         'logs'      => 3, 
+         'diff'      => 3, 
+	 'file'      => 3,
+         'compress'  => 'none' 
+    },
+    'Norbert Bottlaender-Prier' => {
+         'email'     => 'norbert@globenet.org',
+         'summary'   => 1, 
+         'logs'      => 0, 
+         'diff'      => 1, 
+	 'file'      => 0,
+         'compress'  => 'none' 
+    },
+    'Martin Quinson' => {
+         'email'     => 'mquinson@ens-lyon.fr',
+         'summary'   => 3, 
+         'logs'      => 3, 
+         'diff'      => 3, 
+	 'file'      => 0,
+         'compress'  => 'none'
+    },
+    'Frederic Pascal' => {
+         'email'     => 'frederic.pascal@maisel-gw.enst-bretagne.fr',
+         'summary'   => 0, 
+         'logs'      => 0, 
+         'diff'      => 0, 
+	 'file'      => 0,
+         'compress'  => 'none' 
+    },
+    'Jerome Abela' => {
+         'email'     => 'Jerome.Abela@solsoft.fr',
+         'summary'   => 0, 
+         'logs'      => 0, 
+         'diff'      => 0, 
+	 'file'      => 0,
+         'compress'  => 'none' 
+    },
+    'Philippe Caillaud' => {
+         'email'     => 'pcaillaud@infini.fr',
+         'summary'   => 0, 
+         'logs'      => 0, 
+         'diff'      => 0, 
+	 'file'      => 0,
+         'compress'  => 'none' 
+    },
+    'Christophe Le Bars' =>  {
+ 	 'email'     => 'clebars@debian.org',
+         'summary'   => 0, 
+         'logs'      => 0, 
+         'diff'      => 0, 
+	 'file'      => 0,
+         'compress'  => 'none' 
+    },
+    'list' => { # this is a special name containing the default addressee
+         'email'      => 'debian-l10n-french@lists.debian.org',
+         'missing'    => 0, 
+         'summary'    => 2, 
+         'logs'       => 0, 
+         'diff'       => 0, 
+	 'file'       => 0,
+         'compress'   => 'none'
+    }
+};
+return $translators;
+}
+1;
+
+
+
+
+
