561c04484cafce28e7a063fc52081861a8cb4e47
James A. Treacy <treacy@debian.org>
2001-04-20 23:01:14 +0000

    updated

--- webwml/english/mirror/timestamps/mirror_check.py
+++ webwml/english/mirror/timestamps/mirror_check.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import sys, urllib, htmllib, formatter, urlparse, re
+import sys, urllib, htmllib, httplib, formatter, urlparse, re
 
 # given a list of addresses, e.g. 'www.debian.org, www.uk.debian.org',
 # the timestamp files are retrieved and printed out.
@@ -10,6 +10,20 @@ mailto = re.compile('mailto')
 for site in sites:
 	mirror = 'http://' + site + '/mirror/timestamps/'
 	print mirror
+	try:
+		parts = urlparse.urlparse(mirror)
+		h = httplib.HTTP(parts[1])
+		h.putrequest('HEAD', parts[2])
+		h.putheader('Host', parts[1])
+		h.endheaders()
+		errcode, errmsg, headers = h.getreply()
+	except IOError:
+		print '  site does not have a timestamp dir'
+		continue
+	if errcode != 200:
+		print '  Site returned Error Code ' + str(errcode)
+		continue
+	# site must be good so actually download it
 	current = urllib.urlopen(mirror)
 	parse = htmllib.HTMLParser(formatter.NullFormatter())
 	parse.feed(current.read())
