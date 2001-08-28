344fd0b837a40a140cf518012509367aecafaacc
Josip Rodin <joy@debian.org>
2001-08-28 08:15:44 +0000

    use -s in order to skip empty file lists; added -d option to actually touch stuff

--- webwml/touch_old_files.pl
+++ webwml/touch_old_files.pl
@@ -4,7 +4,8 @@
 # languages at the bottom is updated to include the new file.
 # or something like that. made by Joey.
 
-%config = ('wmldir'  => '/org/www.debian.org/webwml',
+%config = (
+	   'wmldir'  => '/org/www.debian.org/webwml',
 	   'datadir' => '/org/www.debian.org/cron/scripts_daily',
 	   );
 
@@ -18,7 +19,8 @@ sub make_listing
 sub obtain_files
 {
     my @list;
-    return if (! -f "$config{'datadir'}/wmlfiles.old" || ! -f "$config{'datadir'}/wmlfiles");
+    return unless -s "$config{'datadir'}/wmlfiles.old";
+    return unless -s "$config{'datadir'}/wmlfiles";
     if (open (IN, "diff -0 $config{'datadir'}/wmlfiles.old $config{'datadir'}/wmlfiles|")) {
 	while (<IN>) {
 	    next until (/^> \.\//);
@@ -31,7 +33,21 @@ sub obtain_files
     return @list;
 }
 
-chdir ($config{'wmldir'});
+chdir ($config{'wmldir'}) || die;
+
+use Getopt::Std;
+$opt_d = 0;
+unless (getopts('d')) {
+	print <<_END_;
+Usage: $0 [-d]
+
+This script finds new translations and touches the old translations so they
+rebuild and include a link to the new ones.
+
+  -d  Remove files, just not report.
+_END_
+	exit;
+}
 
 make_listing ();
 
@@ -40,6 +56,9 @@ make_listing ();
 foreach $file (@new_files) {
     @components = split (/\//, $file);
     shift @components;
-    # printf "%s\t%s\n", $file, join ("/", @components);
-    printf "touch */%s;\n", join ("/", @components);
+    my $cmd = "touch */" . join '/', @components;
+    printf "$cmd\n";
+    if ($opt_d) {
+      system($cmd) == 0 or die;
+    }
 }
