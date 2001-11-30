78c47e3cfaec2b7df650f755a221679f69783325
Denis Barbier <barbier@debian.org>
2001-11-30 00:10:24 +0000

    This new file is a replacement for list-languages, it extracts language list from database file in new format.

---
+++ webwml/english/international/l10n/scripts/list-languages.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -w
+
+#  Extract languages found in data/unstable and store them
+#  in a file
+
+use lib ($0 =~ m|(.*)/|, $1 or ".") ."/../../../../Perl";
+use Webwml::L10n::Db;
+
+my $data = Webwml::L10n::Db->new();
+
+foreach (@ARGV) {
+        $data->read($_);
+}
+
+my $langs = {
+        po              => {},
+        templates       => {},
+        all             => {},
+};
+
+my ($pkg, $line, $file, $lang);
+foreach $pkg ($data->list_packages()) {
+        if ($data->has_po($pkg)) {
+                foreach $line (@{$data->po($pkg)}) {
+                        ($file, $lang) = @{$line};
+                        $langs->{po}->{$lang}  = 1;
+                        $langs->{all}->{$lang} = 1;
+                }
+        }
+        if ($data->has_templates($pkg)) {
+                foreach $line (@{$data->templates($pkg)}) {
+                        ($file, $lang) = @{$line};
+                        $langs->{templates}->{$lang} = 1;
+                        $langs->{all}->{$lang} = 1;
+                }
+        }
+}
+
+foreach my $material (sort keys %$langs) {
+        print "$material: ";
+        print join(" ", sort keys %{$langs->{$material}});
+        print "\n";
+}
+
