0279318ae9a96e062f2cf1b55005e9ec7f5bc9ef
Denis Barbier <barbier@debian.org>
2001-12-07 23:39:00 +0000

    Move all scripts to international/l10n/scripts. Merge gen-po.pl, gen-templates.pl and list-languages.pl into gen-files.pl Add colors in generated tables

---
+++ webwml/english/international/l10n/scripts/fix-files.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+#  Usage: fix-file.sh englishdir lang code1 code2 ...
+
+dir=$1
+lang=$2
+shift; shift
+
+for code
+do
+    test -f $dir/main-$code.inc     || echo 'DELETE-ME' > $dir/main-$code.inc
+    test -f $dir/contrib-$code.inc  || echo 'DELETE-ME' > $dir/contrib-$code.inc
+    test -f $dir/non-free-$code.inc || echo 'DELETE-ME' > $dir/non-free-$code.inc
+    test -f $dir/main-$code.exc     || echo 'DELETE-ME' > $dir/main-$code.exc
+    test -f $dir/contrib-$code.exc  || echo 'DELETE-ME' > $dir/contrib-$code.exc
+    test -f $dir/non-free-$code.exc || echo 'DELETE-ME' > $dir/non-free-$code.exc
+
+    sed -e "s/@tmpl_lang@/$code/" \
+        -e "/LINE: inc-main/r     $dir/main-$code.inc" \
+        -e "/LINE: inc-contrib/r  $dir/contrib-$code.inc" \
+        -e "/LINE: inc-non-free/r $dir/non-free-$code.inc" \
+        -e "/LINE: exc-main/r     $dir/main-$code.exc" \
+        -e "/LINE: exc-contrib/r  $dir/contrib-$code.exc" \
+        -e "/LINE: exc-non-free/r $dir/non-free-$code.exc" \
+            tmpl.$lang.tmpl |\
+        sed -e '/<!-- DO NOT REMOVE THIS LINE/d' |\
+        sed -e ':t /BEGIN SECTION/,/END SECTION/{/END SECTION/!{$!{N;bt};};/DELETE-ME/d;}' \
+                > $code.$lang.html
+done
+
