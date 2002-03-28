54e89d93bd116677bbdac263ce40e89bc57bdf39
Denis Barbier <barbier@debian.org>
2002-03-28 20:30:27 +0000

    Add dfstf

--- webwml/french/international/french/translator.db.pl
+++ webwml/french/international/french/translator.db.pl
@@ -175,6 +175,15 @@ sub init_translators {
                         file        => 0,
                         compress    => 'none'
                 },
+                'dfstf' =>  {
+                        email       => 'dfstf@dsa.tuxfamily.org',
+                        summary     => 2,
+                        logs        => 2,
+                        diff        => 2,
+                        tdiff       => 0,
+                        file        => 0,
+                        compress    => 'none'
+                },
                 # Below are special users, used to handle special cases
                 #     default:      default values
                 #     untranslated: pages not translated
