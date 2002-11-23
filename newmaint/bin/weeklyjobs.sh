04d7f628a3d152f7f0c4e33f6d46fca35c8690b8
Martin Michlmayr <tbm@cyrius.com>
2002-11-23 15:54:00 +0000

    Send the mail as FD.

--- newmaint/bin/weeklyjobs.sh
+++ newmaint/bin/weeklyjobs.sh
@@ -3,5 +3,5 @@
 # 56 23 * * sun /org/nm.debian.org/bin/weeklyjobs.sh
 
 DATE=`date +'AM Report for Week Ending %d %b %Y'`
-/org/nm.debian.org/bin/weekrpt.pl --email | mailx -s "$DATE" debian-newmaint@lists.debian.org
+/org/nm.debian.org/bin/weekrpt.pl --email | mailx -s "$DATE" -a "From: NM Front Desk <new-maintainer@debian.org" debian-newmaint@lists.debian.org
 
