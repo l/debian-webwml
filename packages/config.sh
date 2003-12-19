fb1c1d7f17230eaf7738101644968a1001f36bf7
Joey Schulze <joey@debian.org>
2003-12-19 07:38:06 +0000

    Added architectures and distributions so that they're configurable as well

--- packages/config.sh
+++ packages/config.sh
@@ -18,3 +18,9 @@ ftpsite=http://ftp.debian.org/debian
 nonus_ftpsite=http://ftp.uk.debian.org/debian-non-US
 security_ftpsite=http://security.debian.org/debian-security
 
+# Architectures
+#
+dists="stable testing unstable"
+arch_stable="alpha arm hppa i386 ia64 m68k mips mipsel powerpc s390 sparc"
+arch_testing="${arch_stable}"
+arch_unstable="alpha arm hppa hurd-i386 i386 ia64 m68k mips mipsel powerpc s390 sparc"
