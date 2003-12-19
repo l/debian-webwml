378296068545d9985b22a86617c1be94419822e4
Joey Schulze <joey@debian.org>
2003-12-19 07:14:47 +0000

    Configuration file, simply copied from djpig

---
+++ packages/config.sh
@@ -0,0 +1,20 @@
+# Configuration for packages.debian.org
+#
+
+topdir=/src/work/packages
+
+bindir=${topdir}/bin
+filesdir=${topdir}/files
+swishdir=${topdir}/swish++
+htmldir=${topdir}/www
+archivedir=${topdir}/archive
+
+# unset this if packages.debian.org moves somewhere where the packages files
+# cannot be obtained locally
+#
+localdir=/org/ftp.debian.org/ftp
+
+ftpsite=http://ftp.debian.org/debian
+nonus_ftpsite=http://ftp.uk.debian.org/debian-non-US
+security_ftpsite=http://security.debian.org/debian-security
+
