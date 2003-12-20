a646be6655a470ec111887871adf9527f070b91c
Frank Lichtenheld <djpig@debian.org>
2003-12-20 16:55:56 +0000

    Add $parts and $arch_experimental

--- packages/config.sh
+++ packages/config.sh
@@ -20,9 +20,11 @@ security_ftpsite=http://security.debian.org/debian-security
 
 # Architectures
 #
+parts="main contrib non-free"
 dists="stable testing unstable"
 arch_stable="alpha arm hppa i386 ia64 m68k mips mipsel powerpc s390 sparc"
 arch_testing="${arch_stable}"
 arch_unstable="alpha arm hppa hurd-i386 i386 ia64 m68k mips mipsel powerpc s390 sparc"
+arch_experimental="${arch_unstable}"
 arch_testing_proposed_updates="${arch_testing}"
 arch_stable_proposed_updates="${arch_stable}"
