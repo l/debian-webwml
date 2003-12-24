2785c9913eab11dc39883e6b444996877b786805
Frank Lichtenheld <djpig@debian.org>
2003-12-24 01:15:23 +0000

    Some more stuff (for HTML generation)

---
+++ packages/lib/Packages/HTML.pm
@@ -0,0 +1,185 @@
+package Packages::HTML;
+
+use strict;
+use warnings;
+
+use Packages::I18N::Locale;
+use Packages::I18N::Languages;
+use Packages::I18N::LanguageNames;
+
+our @ISA = qw( Exporter );
+our @EXPORT = qw( header footer );
+
+our $HOME = "http://www.debian.org/";
+our $CN_HELP_URL = "$HOMEintro/cn";
+
+my %img_trans = ( pt_BR => "pt", pt_PT => "pt", sv_SE => "sv" );
+
+sub img {
+    my ( $root, $url, $src, $alt, %attr ) = @_; 
+    my @attr;
+
+    foreach my $a ( keys %attr ) {
+	push @attr, "$a=\"$attr{$a}\"";
+    }
+
+    return "<a href=\"$root$url\"><img src=\"$root$src\" border=\"0\" alt=\"$alt\" @attr></a>";
+}
+
+sub header {
+    my ($TITLE, $PACKTOP, $ROOT, $LANG, $DESC, $KEYWORDS) = @_;
+    my $DESC_LINE;
+    if (defined $DESC) {
+	$DESC =~ s/"/&quot;/g; # " <- Make syntax highlighting happy
+	$DESC_LINE = "<meta name=\"Description\" content=\"$DESC\">";
+    }
+    else {
+	$DESC_LINE = '';
+    }
+    if (!defined $KEYWORDS) {
+	$KEYWORDS = '';
+    }
+    my $KEYWORDS_LINE = "<meta name=\"Keywords\" content=\"debian, $KEYWORDS $TITLE\">";
+    
+    my $img_lang = $img_trans{$LANG} || $LANG;
+    my $charset = get_charset($LANG);
+    my $txt = <<HEAD;
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html lang="$LANG">
+<head>
+<title>Debian GNU/Linux -- $TITLE</title>
+<link rev="made" href="mailto:webmaster\@debian.org">
+<meta http-equiv="Content-Type" content="text/html; charset=$charset">
+<meta name="Author" content="Debian Webmaster, webmaster\@debian.org">
+$KEYWORDS_LINE
+$DESC_LINE
+</head>
+<body text="#000000" bgcolor="#FFFFFF" link="#0000FF" vlink="#800080" alink="#FF0000">
+<table border="0" cellpadding="3" cellspacing="0" align="center" summary="">
+<tr>
+<td>
+<a href="$HOME/"><img src="$HOME/logos/openlogo-nd-50.png" border="0" hspace="0" vspace="0" alt="" width="50" height="61"></a>
+HEAD
+;
+
+    $txt .= img( "$HOME/", "", "Pics/debian.jpg", gettext( "Debian Project" ),
+		 width => 179, height => 61 );
+    $txt .= <<NAVBEGIN;
+</td>
+</tr>
+</table>
+<table bgcolor="#DF0451" border="0" cellpadding="0" cellspacing="0" width="100%" summary="">
+<tr>
+<td valign="top">
+<img src="$HOME/Pics/red-upperleft.png" align="left" border="0" hspace="0" vspace="0" alt="" width="15" height="16">
+</td>
+<td rowspan="2" align="center">
+NAVBEGIN
+;
+
+    $txt .= img( "$HOME/", "intro/about", "Pics/about.$img_lang.gif",
+		 gettext( "About&nbsp;Debian" ), 
+		 hspace => 4, vspace => 7 );
+    $txt .= img( "$HOME/", "News/", "Pics/news.$img_lang.gif",
+		 gettext( "News" ), 
+		 hspace => 4, vspace => 7 );
+    $txt .= img( "$HOME/", "distrib", "Pics/getting.$img_lang.gif",
+		 gettext( "Getting&nbsp;Debian" ), 
+		 hspace => 4, vspace => 7 );
+    $txt .= img( "$HOME/", "support/", "Pics/support.$img_lang.gif",
+		 gettext( "Support" ), 
+		 hspace => 4, vspace => 7 );
+    $txt .= img( "$HOME/", "devel/", "Pics/devel.$img_lang.gif",
+		 gettext( "Development" ), 
+		 hspace => 4, vspace => 7 );
+    $txt .= img( "$HOME/", "sitemap", "Pics/sitemap.$img_lang.gif",
+		 gettext( "Site map" ), 
+		 hspace => 4, vspace => 7 );
+    $txt .= img( "", "http://search.debian.org/", 
+		 "$HOME/Pics/search.$img_lang.gif",
+		 gettext( "Search" ), 
+		 hspace => 4, vspace => 7 );
+    
+    $txt .= <<ENDNAV;
+</td>
+<td valign="top">
+<img src="$HOME/Pics/red-upperright.png" align="right" border="0" hspace="0" vspace="0" alt="" width="15" height="16">
+</td>
+</tr>
+<tr>
+<td valign="bottom">
+<img src="$HOME/Pics/red-lowerleft.png" align="left" border="0" hspace="0" vspace="0" alt="" width="16" height="16">
+</td>
+<td valign="bottom">
+<img src="$HOME/Pics/red-lowerright.png" align="right" border="0" hspace="0" vspace="0" alt="" width="15" height="16">
+</td>
+</tr>
+</table>
+ENDNAV
+;
+
+# if there is no fifth parameter, this must be some sort of an index page
+# so we can add a heading with the title
+    if (!defined $DESC) {
+	$txt .= "<h1>$TITLE</h1>\n\n";
+    }
+
+    return $txt;
+}
+
+sub trailer {
+    my ($ROOT, $NAME, $LANG, @USED_LANGS) = @_;
+    my $txt = languages( $NAME, $LANG, @USED_LANGS );
+    $txt .=
+	"\n\n<hr noshade width=\"100%\" size=\"1\">" .
+	sprintf( gettext( "Back to: <a href=\"%s/\">Debian Project homepage</a> || <a href=\"%s/\">Packages search page</a>" ), $HOME, $ROOT ).
+	"<hr noshade width=\"100%\" size=\"1\">".
+	"<small>".
+	sprintf( gettext( "See the Debian <a href=\"%s/contact\">contact page</a> for information on contacting us." ), $HOME ).
+	"</small>".
+	"<p><small>". gettext( "Last Modified: " ). "LAST_MODIFIED_DATE".
+	"<br>".
+	sprintf( gettext( "Copyright &copy; 1997-2003 <a href=\"http://www.spi-inc.org\">SPI</a>; See <a href=\"%s/license\">license terms</a>." ), $HOME )."<br>".
+	gettext( "Debian is a registered trademark of Software in the Public Interest, Inc." ).
+	"</small>".
+	"</body>\n</html>";
+
+    return $txt;
+}
+
+sub languages {
+    my ( $name, $lang, @used_langs ) = @_;
+    
+    my $str = "";
+    
+    if (@used_langs) {
+	$str .= "<hr>\n";
+	$str .= "<!--UdmComment-->\n";
+	$str .= gettext( "This page is also available in the following languages:\n" );
+	$str .= "<br>\n";
+	
+	%sorted_langs = ();
+	foreach (@used_langs) {
+	    next if $_ eq $lang; # Never print the current language
+	    unless ($selflang{$_}) { warn "missing language $_"; next } #DEBUG
+	    $sorted_langs{$selflang{$_}} = $_;
+	}
+	return "" unless scalar keys %sorted_langs;
+	# Sort on uppercase to work with languages which use lowercase initial
+	# letters.
+	foreach (sort langcmp keys %sorted_langs) {
+	    my $cur_lang = $sorted_langs{$_};
+	    my $tooltip = dgettext( "langs", get_language_name($cur_lang) );
+	    $str .= "<a href=\"$name.$cur_lang.html\" title=\"$tooltip\" hreflang=\"$cur_lang\" lang=\"$cur_lang\" rel=\"alternate\">$_";
+	    $str .= "&nbsp;(get_transliteration($cur_lang))" if defined get_transliteration($cur_lang);
+	    $str .= "</a>&nbsp;\n";
+	}
+	$str .= "\n<br />\n";
+	$str .= gettext( "How to set <a href=\"$CN_HELP_URL\">the default document language</a>" );
+	$str .= "\n<!--/UdmComment-->\n";
+    }
+    
+    return $str;
+}
+
+1;
