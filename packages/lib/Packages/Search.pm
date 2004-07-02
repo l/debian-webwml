cb270524eaf826b746fb915307a2195395a24e13
Frank Lichtenheld <djpig@debian.org>
2004-07-02 11:52:01 +0000

    Rework the page selection code and move most of the functionality to Packages::Search

--- packages/lib/Packages/Search.pm
+++ packages/lib/Packages/Search.pm
@@ -45,11 +45,27 @@ use strict;
 use warnings;
 
 use CGI;
+use POSIX;
 
 use Deb::Versions;
+use Exporter;
+
+our @ISA = qw( Exporter );
+
+our @EXPORT_OK = qw( nextlink prevlink indexline
+                     resperpagelink );
+our %EXPORT_TAGS = ( all => [ @EXPORT_OK ] );
 
 our $VERSION = 0.01;
 our $RCS_VERSION = '$Revision$';
+
+our $USE_PAGED_MODE = 1;
+use constant DEFAULT_PAGE => 1;
+use constant DEFAULT_RES_PER_PAGE => 50;
+our %page_params = ( page => { default => DEFAULT_PAGE,
+                               match => '(\d+)' },
+                     number => { default => DEFAULT_RES_PER_PAGE,
+                                 match => '(\d+)' } );
 
 my $debug = 0;
 
@@ -57,14 +73,26 @@ sub parse_params {
     my ( $cgi, $params_def ) = @_;
 
     my %params_ret = ( values => {}, errors => {} );
+    my %params;
+    if ($USE_PAGED_MODE) {
+        print "DEBUG: Use PAGED_MODE<br>" if $debug;
+        %params = %$params_def;
+        foreach (keys %page_params) {
+            delete $params{$_};
+        }
+        %params = ( %params, %page_params );
+    } else {
+        %params = %$params_def;
+    }
 
-    foreach my $param ( keys %$params_def ) {
+    foreach my $param ( keys %params ) {
 	
 	print "DEBUG: Param $param<br>" if $debug;
 
 	my $p_value_orig = $cgi->param($param);
 	my $p_value = $p_value_orig;
 
+        #TODO: don't produce "uninitialized value" warnings
 	print "DEBUG: Value (Orig) $p_value_orig<br>" if $debug;
 
 	if ($params_def->{$param}{match} && defined $p_value_orig) {
@@ -74,8 +102,8 @@ sub parse_params {
 	print "DEBUG: Value (Match) $p_value<br>" if $debug;
 
 	unless (defined $p_value) {
-	    if (defined $params_def->{$param}{default}) {
-		$p_value = $params_def->{$param}{default};
+	    if (defined $params{$param}{default}) {
+		$p_value = $params{$param}{default};
 	    } else {
 		$params_ret{errors}{$param} = "undef";
 		next;
@@ -85,10 +113,10 @@ sub parse_params {
 	print "DEBUG: Value (Default) $p_value<br>" if $debug;
 	my $p_value_no_replace = $p_value;
 
-	if ($params_def->{$param}{replace} && $p_value) {
-	    foreach (keys %{$params_def->{$param}{replace}}) {
+	if ($params{$param}{replace} && $p_value) {
+	    foreach (keys %{$params{$param}{replace}}) {
 		if ($p_value eq $_) {
-		    $p_value = $params_def->{$param}{replace}{$_};
+		    $p_value = $params{$param}{replace}{$_};
 		}
 	    }
 	}
@@ -100,7 +128,105 @@ sub parse_params {
 	    final => $p_value,
 	};
     }
+
+    if ($USE_PAGED_MODE) {
+        $cgi->delete( "page", "number" );
+    }
+
     return %params_ret;
 }
+
+sub start { 
+    my $params = shift;
+
+    my $page = $params->{values}{page}{final}
+    || DEFAULT_PAGE;
+    my $res_per_page = $params->{values}{number}{final}
+    || DEFAULT_RES_PER_PAGE;
+
+    return $res_per_page * ($page - 1) + 1;
+}
+
+sub end {
+    my $params = shift;
+
+    my $page = $params->{values}{page}{final}
+    || DEFAULT_PAGE;
+    $page++;
+    my $res_per_page = $params->{values}{number}{final}
+    || DEFAULT_RES_PER_PAGE;
+
+    return $page * $res_per_page + 1;
+}
+
+sub indexline {
+    my ($cgi, $params, $num_res) = @_;
+
+    my $index_line = "";
+    my $page = $params->{values}{page}{final}
+    || DEFAULT_PAGE;
+    my $res_per_page = $params->{values}{number}{final}
+    || DEFAULT_RES_PER_PAGE;
+    my $numpages = ceil($num_res /
+                        $res_per_page);
+    for (my $i = 1; $i <= $numpages; $i++) {
+        if ($i == $page) {
+            $index_line .= $i;
+        } else {
+            $index_line .= "<a href=\"".$cgi->self_url.
+                "&page=$i&number=$res_per_page\">".
+                "$i</a>";
+        }
+	if ($i < $numpages) {
+	   $index_line .= " | ";
+	}
+    }
+    return $index_line;
+}
+
+sub nextlink {
+    my ($cgi, $params, $no_results ) = @_;
+
+    my $page = $params->{values}{page}{final}
+    || DEFAULT_PAGE;
+    $page++;
+    my $res_per_page = $params->{values}{number}{final}
+    || DEFAULT_RES_PER_PAGE;
+
+    if ((($page-1)*$res_per_page + 1) > $no_results) {
+        return "&gt;&gt;";
+    }
+
+    return "<a href=\"".$cgi->self_url.
+        "&page=$page&number=$res_per_page\">&gt;&gt;</a>";
+}
+
+sub prevlink {
+    my ($cgi, $params ) = @_;
+
+    my $page = $params->{values}{page}{final}
+    || DEFAULT_PAGE;
+    $page--;
+    if (!$page) {
+        return "&lt;&lt;";
+    }
+
+    my $res_per_page = $params->{values}{number}{final}
+    || DEFAULT_RES_PER_PAGE;
+
+    return "<a href=\"".$cgi->self_url.
+        "&page=$page&number=$res_per_page\">&lt;&lt;</a>";
+}
+
+sub resperpagelink {
+    my ($cgi, $params, $res_per_page ) = @_;
+
+    my $start = start( $params );
+    my $page = ceil($start / $res_per_page);
+
+    return "<a href=\"".$cgi->self_url.
+        "&page=$page&number=$res_per_page\">$res_per_page</a>";
+}
+
 
 1;
