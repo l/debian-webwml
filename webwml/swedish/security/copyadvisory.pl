db6e773e8bbe36afacf1a7ba9e2b795972db9378
Peter Krefting <peterk@debian.org>
2005-01-25 15:42:02 +0000

    More variant spellings.

--- webwml/swedish/security/copyadvisory.pl
+++ webwml/swedish/security/copyadvisory.pl
@@ -155,7 +155,7 @@ while (<SRC>)
 	s/\(sarge\)/(Sarge)/;
 	s/\(sid\)/(Sid)/;
 	s/Refer to Debian (<.*>)?bug #([0-9]+)</Se Debians $1felrapport $2</;
-	s/of the Debian Security Audit Project/från Debians säkerhetsgranskningsprojekt/;
+	s/(of|from) the Debian Security Audit (Project|Team)/från Debians säkerhetsgranskningsprojekt/;
 
 	print DST $_;
 }
