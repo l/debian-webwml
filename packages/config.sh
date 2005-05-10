62883a94895bd1ac2f49798283e33f43b80990d2
Frank Lichtenheld <djpig@debian.org>
2005-05-10 21:51:25 +0000

    Merge from experimental:  * Add AMD64 archive    - cgi-bin/download.pl    - config.sh    - cron/1syncarchive_amd64  * Add volatile archive but don't use it yet    - cgi-bin/download.pl    - config.sh    - cron/1syncarchive_volatile    - cron/3extract_archive    - cron/3extract_packages    - cron/3extract_sources    - cron/9update_pages    - htmlscripts/create_package_pages    - htmlscripts/pages.pl    - lib/Packages/HTML.pm  * Completly rewritten changelog/copyright extraction (sh->Perl),    adds HTML version of the changelog    - cron/3extract_changelog    - bin/extract_files    (May address #296872 and #297040)  * Code clean-up in RSS generating code    - htmlscripts/create_newpkg  * New download table which is better readable in text-based browsers    - htmlscripts/pages.pl    (Patch by Jutta Wrage) (Partly addresses #290187)  * Various smaller fixes

--- packages/config.sh
+++ packages/config.sh
@@ -8,7 +8,6 @@ bindir=${topdir}/bin
 scriptdir=${topdir}/htmlscripts
 libdir=${topdir}/lib
 filesdir=${topdir}/files
-swishdir=${topdir}/swish++
 htmldir=${topdir}/www
 archivedir=${topdir}/archive
 podir=${topdir}/po
@@ -25,6 +24,8 @@ ftproot=/org/ftp.root
 ftpsite=http://ftp.debian.org/debian
 nonus_ftpsite=http://ftp.uk.debian.org/debian-non-US
 security_ftpsite=http://security.debian.org/debian-security
+volatile_ftpsite=http://volatile.debian.net/debian-volatile
+amd64_ftpsite=http://amd64.debian.net/debian
 
 # Architectures
 #
@@ -33,8 +34,8 @@ ddtplangs="de cs da eo es fi fr hu it ja nl pl pt_BR pt_PT ru sk sv_SE uk"
 parts="main contrib non-free"
 dists="stable testing unstable"
 arch_stable="alpha arm hppa i386 ia64 m68k mips mipsel powerpc s390 sparc"
-arch_testing="${arch_stable}"
-arch_unstable="alpha arm hppa hurd-i386 i386 ia64 m68k mips mipsel powerpc s390 sparc"
+arch_testing="${arch_stable} amd64"
+arch_unstable="alpha arm hppa hurd-i386 i386 ia64 m68k mips mipsel powerpc s390 sparc amd64"
 arch_experimental="${arch_unstable}"
 arch_testing_proposed_updates="${arch_testing}"
 arch_stable_proposed_updates="${arch_stable}"
