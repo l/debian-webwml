d1eb939c67bf669c72a57f154300a6472a29c01e
Joey Schulze <joey@debian.org>
2005-05-28 11:44:41 +0000

    Looks like one should spell the locale properly

--- webwml/german/News/weekly/makemail.pl
+++ webwml/german/News/weekly/makemail.pl
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-LC_MESSAGES=C LC_CTYPE=de_DE.ISO-8859-15 ../../../english/News/weekly/makemail.pl "$@"
+LC_MESSAGES=C LC_CTYPE=de_DE.ISO-8859-15@euro ../../../english/News/weekly/makemail.pl "$@"
