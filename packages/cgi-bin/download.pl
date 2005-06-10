b076216975b676038141c4d0ae67949be1ea0d58
Frank Lichtenheld <djpig@debian.org>
2005-06-10 22:30:38 +0000

    Replace nearly the whole script (other mirror list, remove unneeded texts, etc)

--- packages/cgi-bin/download.pl
+++ packages/cgi-bin/download.pl
@@ -19,143 +19,61 @@ use Packages::HTML ();
 my ($input,   # The CGI data
     $file, $filen, $md5sum, @file_components, $type, $arch);
 
-# TODO: find a way to get the U.S. mirror list from a more authoritive
-# location automatically. might not be overly smart to automatize it
-# completely, since I hand pick sites that are up-to-date, fast, and
-# have HTTP on a reasonably short URL
-#   -- Joy
-
-# hint:
-# grep-dctrl -F Site,Alias -e '(udel|bigfoot|kernel|crosslink|internap|cerias|lcs.mit|progeny)' Mirrors.masterlist | timestamps/archive_mirror_check.py
-my @north_american_sites = (
-	"ftp.us.debian.org/debian",
-	"http.us.debian.org/debian",
-	"ftp.debian.org/debian",
-#	"ftp.ca.debian.org/debian",
-	"ftp.egr.msu.edu/debian",
-	"mirrors.kernel.org/debian",
-	"archive.progeny.com/debian",
-	"debian.crosslink.net/debian",
-	"ftp-mirror.internap.com/pub/debian",
-	"ftp.cerias.purdue.edu/pub/os/debian",
-	"ftp.lug.udel.edu/debian",
-	"debian.lcs.mit.edu/debian",
-	"debian.teleglobe.net",
-	"debian.rutgers.edu",
-	"debian.oregonstate.edu/debian",
+my @north_american_sites = qw(
+	ftp.cs.umn.edu/pub/ubuntu
+	mirror.clarkson.edu/pub/distributions/ubuntu
+	ubuntu.mirrors.tds.net/ubuntu
+	itanix.rutgers.edu/ubuntu
+	www.opensourcemirrors.org/ubuntu
+	ftp.ale.org/pub/mirrors/ubuntu
+	ubuntu.secs.oakland.edu
 	);
-my @european_sites = (
-	"ftp.de.debian.org/debian",
-	"ftp.at.debian.org/debian",
-	"ftp.bg.debian.org/debian",
-	"ftp.cz.debian.org/debian",
-	"ftp.dk.debian.org/debian",
-	"ftp.ee.debian.org/debian",
-	"ftp.fi.debian.org/debian",
-	"ftp.fr.debian.org/debian",
-	"ftp.hr.debian.org/debian",
-	"ftp.hu.debian.org/debian",
-	"ftp.ie.debian.org/debian",
-	"ftp.is.debian.org/debian",
-	"ftp.it.debian.org/debian",
-	"ftp.nl.debian.org/debian",
-	"ftp.no.debian.org/debian",
-	"ftp.pl.debian.org/debian",
-	"ftp.si.debian.org/debian",
-	"ftp.es.debian.org/debian",
-	"ftp.se.debian.org/debian",
-	"ftp.tr.debian.org/debian",
-	"ftp.uk.debian.org/debian",
+my @european_sites = qw(
+	archive.ubuntu.com/ubuntu
+	ubuntu.inode.at/ubuntu
+	ubuntu.uni-klu.ac.at/ubuntu
+	gd.tuwien.ac.at/opsys/linux/ubuntu/archive
+	ftp.belnet.be/pub/mirror/ubuntu.com
+	mirror.freax.be/ubuntu/archive.ubuntu.com
+	archive.ubuntu.cz/ubuntu
+	mirrors.dk.telia.net/ubuntu
+	mirrors.dotsrc.org/ubuntu
+	mir1.ovh.net/ubuntu/ubuntu
+	ftp.inf.tu-dresden.de/os/linux/dists/ubuntu
+	www.artfiles.org/ubuntu.com/archive
+	ftp.rz.tu-bs.de/pub/mirror/ubuntu-packages
+	ftp.join.uni-muenster.de/pub/mirrors/ftp.ubuntu.com/ubuntu
+	ftp.kfki.hu/linux/ubuntu
+	ubuntu.odg.cc
+	ftp.esat.net/mirrors/archive.ubuntu.com
+	ftp.heanet.ie/pub/ubuntu
+	na.mirror.garr.it/mirrors/ubuntu-archive
+	ftp.litnet.lt/pub/ubuntu
+	ubuntu.synssans.nl
+	ubuntulinux.mainseek.com/ubuntu
+	ftp.acc.umu.se/mirror/ubuntu
+	mirror.switch.ch/ftp/mirror/ubuntu
+	www.mirrorservice.org/sites/archive.ubuntu.com/ubuntu
 	);
-my @south_american_sites = (
-	"ftp.br.debian.org/debian",
-	"ftp.cl.debian.org/debian",
+my @south_american_sites = qw(
+	ftp.interlegis.gov.br/pub/ubuntu/archive
+        ubuntu.c3sl.ufpr.br
 	);
-my @australian_sites = (
-	"ftp.au.debian.org/debian",
-	"ftp.wa.au.debian.org/debian",
-	"ftp.nz.debian.org/debian",
+my @australian_sites = qw(
+	mirror.isp.net.au/ftp/pub/ubuntu
+	www.planetmirror.com/pub/ubuntu
 	);
-my @asian_sites = (
-	"ftp.jp.debian.org/debian",
-#	"ftp.kr.debian.org/debian",
-	"linux.csie.nctu.edu.tw/debian",
-	"debian.linux.org.tw/debian",
-	"linux.cdpa.nsysu.edu.tw/debian",
+my @asian_sites = qw(
+	archive.ubuntu.org.cn/ubuntu
+	komo.vlsm.org/ubuntu
+	kambing.vlsm.org/ubuntu
+	ubuntu.csie.ntu.edu.tw/ubuntu
 	); 
-
-my @volatile_european_sites = (
-        "volatile.debian.net/debian-volatile",
-        "ftp2.de.debian.org/debian-volatile",
-        "ftp.sk.debian.org/debian-volatile",
-			       );
-my @amd64_european_sites = (
-        "amd64.debian.net/debian-amd64/debian",
-        "ftp.de.debian.org/debian-amd64/debian",
-        "bach.hpc2n.umu.se/debian-amd64/debian",
-        "bytekeeper.as28747.net/debian-amd64/debian",
-	"mirror.switch.ch/debian-amd64/debian",
-        "ftp.nl.debian.org/debian-amd64/debian",
-			    );
-my @amd64_asian_sites = (
-        "hanzubon.jp/debian-amd64/debian",
-			    );
-my @amd64_north_american_sites = (
-        "mirror.espri.arizona.edu/debian-amd64/debian",
-			    );
-my @nonus_north_american_sites = (
-#	"ftp.ca.debian.org/debian-non-US",
-	"debian.yorku.ca/debian/non-US",
-	"mirror.direct.ca/linux/debian-non-US",
-	);
-my @nonus_european_sites = (
-	"non-us.debian.org/debian-non-US",
-	"ftp.de.debian.org/debian-non-US",
-	"ftp.at.debian.org/debian-non-US",
-	"ftp.bg.debian.org/debian-non-US",
-	"ftp.cz.debian.org/debian-non-US",
-	"ftp.fi.debian.org/debian-non-US",
-	"ftp.fr.debian.org/debian-non-US",
-	"ftp.hr.debian.org/debian-non-US",
-	"ftp.hu.debian.org/debian-non-US",
-	"ftp.ie.debian.org/debian-non-US",
-	"ftp.is.debian.org/debian-non-US",
-	"ftp.it.debian.org/debian-non-US",
-	"ftp.nl.debian.org/debian-non-US",
-	"ftp.no.debian.org/debian-non-US",
-	"ftp.pl.debian.org/debian/non-US",
-	"ftp.si.debian.org/debian-non-US",
-	"ftp.es.debian.org/debian-non-US",
-	"ftp.se.debian.org/debian-non-US",
-	"ftp.tr.debian.org/debian-non-US",
-	"ftp.uk.debian.org/debian/non-US",
-	);
-my @nonus_australian_sites = (
-	"ftp.au.debian.org/debian-non-US",
-	"ftp.wa.au.debian.org/debian-non-US",
-	"ftp.nz.debian.org/debian-non-US",
-	);
-my @nonus_asian_sites = (
-	"ftp.jp.debian.org/debian-non-US",
-#	"ftp.kr.debian.org/debian-non-US",
-	"linux.csie.nctu.edu.tw/debian-non-US",
-	"debian.linux.org.tw/debian-non-US",
-	"linux.cdpa.nsysu.edu.tw/debian-non-US",
-	);
-my @nonus_south_american_sites = (
-	"ftp.br.debian.org/debian-non-US",
-	"ftp.cl.debian.org/debian-non-US",
-	);
 
 # list of architectures
 my %arches = (
         i386    => 'Intel x86',
-        m68k    => 'Motorola 680x0',
-        sparc   => 'SPARC',
-        alpha   => 'Alpha',
         powerpc => 'PowerPC',
-        arm     => 'ARM',
-        hppa    => 'HP PA-RISC',
         ia64    => 'Intel IA-64',
         mips    => 'MIPS',
         mipsel  => 'MIPS (DEC)',
@@ -207,56 +125,8 @@ if ($type eq 'security') {
 
 	print <<END;
 <ul>
-  <li><a href="http://security.debian.org/debian-security/$file">security.debian.org/debian-security</a></li>
+  <li><a href="http://security.ubuntu.com/ubuntu/$file">security.ubuntu.com/ubuntu</a>
 </ul>
-
-<p>Debian security updates are currently officially distributed only via
-security.debian.org.</p>
-END
-
-} elsif ($arch eq 'amd64') {
-
-    print_links( "North America", $file, @amd64_north_american_sites );
-    print_links( "Europe", $file, @amd64_european_sites );
-#    print_links( "Australia and New Zealand", $file,
-#		 @nonus_australian_sites );
-    print_links( "Asia", $file, @amd64_asian_sites );
-#    print_links( "South America", $file, @nonus_south_american_sites );
-
-	print <<END;
-  <p>Note that AMD64 is not officialy included in the Debian archive
-  yet, but the AMD64 porter group keeps their archive in sync with
-  the official archive as close as possible. See the
-  <a href="http://www.debian.org/ports/amd64/">AMD64 ports page</a> for
-  current information.</p>
-END
-
-} elsif ($type eq 'nonus') {
-
-    print_links( "North America", $file, @nonus_north_american_sites );
-    print_links( "Europe", $file, @nonus_european_sites );
-    print_links( "Australia and New Zealand", $file,
-		 @nonus_australian_sites );
-    print_links( "Asia", $file, @nonus_asian_sites );
-    print_links( "South America", $file, @nonus_south_american_sites );
-
-    print <<END;
-<p>If none of the above sites are fast enough for you, please see our
-<a href="http://www.debian.org/mirror/list-non-US">complete mirror list</a>.</p>
-END
-
-} elsif ($type eq 'volatile') {
-
-#    print_links( "North America", $file, @nonus_north_american_sites );
-    print_links( "Europe", $file, @volatile_european_sites );
-#    print_links( "Australia and New Zealand", $file,
-#		 @nonus_australian_sites );
-#    print_links( "Asia", $file, @nonus_asian_sites );
-#    print_links( "South America", $file, @nonus_south_american_sites );
-
-    print <<END;
-<p>If none of the above sites are fast enough for you, please see our
-<a href="http://volatile.debian.net/mirrors.html">complete mirror list</a>.</p>
 END
 
 } elsif ($type eq 'main') {
@@ -273,9 +143,11 @@ END
     print_links( "South America", $file, @south_american_sites );
     print "</td></tr></table>";
 
-    print <<END;
-<p>If none of the above sites are fast enough for you, please see our
-<a href="http://www.debian.org/mirror/list">complete mirror list</a>.</p>
+	print <<END;
+<p>If none of the above sites are fast enough for you, please see the
+<a href="http://www.ubuntulinux.org/wiki/Archive">complete mirror list</a>.<br>
+The list of mirrors used by this scripts was last synced with the mirror list
+<em>Wed, 22 Dec 2004 01:32:12 +0000</em>.</p>
 END
 }
 
