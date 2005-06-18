7e26cb22609490f82d1e46eb190cff799b3aeba8
Thomas Huriaux <thomas.huriaux@enst-bretagne.fr>
2005-06-18 12:19:12 +0000

    intl/french is now a normal directory

--- webwml/remove_stale.pl
+++ webwml/remove_stale.pl
@@ -47,8 +47,8 @@ sub recurse
 	# Get parameter.
 	my $directory = shift;
 
-	# Don't try to do anything in l10n or intl/french directories.
-	return 0 if $directory =~ /l10n$/ or $directory =~ /ional\/french$/ or $directory =~ /ional\/portuguese$/;
+	# Don't try to do anything in l10n or intl/portuguese directories.
+	return 0 if $directory =~ /l10n$/ or $directory =~ /ional\/portuguese$/;
 
 	# Load all entries for this directory.
 	opendir THISDIR, $directory
