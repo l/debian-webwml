f9a846165f5cd56b2fefecc7b6881ada54f2e533
Frank Lichtenheld <djpig@debian.org>
2005-08-15 13:43:33 +0000

    While we should ignore sub directories of l10n/, we shouldn't do so with the directory itself

--- webwml/remove_stale.pl
+++ webwml/remove_stale.pl
@@ -47,8 +47,8 @@ sub recurse
 	# Get parameter.
 	my $directory = shift;
 
-	# Don't try to do anything in l10n.
-	return 0 if $directory =~ /l10n$/;
+	# Don't try to do anything in sub directories of l10n.
+	return 0 if $directory =~ m'l10n/[^/]+$';
 
 	# Load all entries for this directory.
 	opendir THISDIR, $directory
