048bf0a80cb481bad29bdbe8c01f3ddb55c368d9
cvs2git <>
2005-09-07 14:58:14 +0000

    This commit was manufactured by cvs2svn to create branch 'ubuntu'.
    Cherrypick from master 2005-09-07 14:58:13 UTC Frank Lichtenheld <djpig@debian.org> 'Fix typo in description of debian-installer section':
        packages/cron/7install_static
        packages/etc/tmpl/content.tmpl
        packages/etc/tmpl/default.tmpl
        packages/etc/tmpl/footer.tmpl
        packages/etc/tmpl/header.tmpl
        packages/etc/tmpl/html_head.tmpl
        packages/lib/Parse/DebianChangelog.pm
        packages/lib/Parse/DebianChangelog/ChangesFilters.pm
        packages/lib/Parse/DebianChangelog/Entry.pm
        packages/lib/Parse/DebianChangelog/Util.pm
        packages/po/langs.fr.po
        packages/po/pdo.fr.po
        packages/po/sections.fr.po
        packages/static/Pics/adep.gif
        packages/static/Pics/dep.gif
        packages/static/Pics/idep.gif
        packages/static/Pics/rec.gif
        packages/static/Pics/sug.gif
        packages/static/changelogs-print.css
        packages/static/changelogs.css
        packages/static/debian.css

---
+++ packages/lib/Parse/DebianChangelog/Util.pm
@@ -0,0 +1,133 @@
+#
+# Parse::DebianChangelog::Util
+#
+# Copyright 1996 Ian Jackson
+# Copyright 2005 Frank Lichtenheld <frank@lichtenheld.de>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+=head1 NAME
+
+Parse::DebianChangelog::Util - utility functions for parsing Debian changelogs
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+This is currently only used internally by Parse::DebianChangelog and
+is not yet documented. There may be still API changes until this module
+is finalized.
+
+=cut
+
+package Parse::DebianChangelog::Util;
+
+use strict;
+use warnings;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+our %EXPORT_TAGS = ( 'all' => [ qw(
+		 find_closes
+		 data2rfc822
+		 data2rfc822_mult
+		 get_dpkg_changes
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+);
+
+sub find_closes {
+    my $changes = shift;
+    my @closes = ();
+
+    while ($changes && ($changes =~ /closes:\s*(?:bug)?\#?\s?\d+(?:,\s*(?:bug)?\#?\s?\d+)*/ig)) {
+	push(@closes, $& =~ /\#?\s?(\d+)/g);
+    }
+
+    @closes = sort { $a <=> $b } @closes;
+    return \@closes;
+}
+
+sub data2rfc822_mult {
+    my ($data, $fieldimps) = @_;
+    my @rfc822 = ();
+
+    foreach my $entry (@$data) {
+	push @rfc822, data2rfc822($entry,$fieldimps);
+    }
+
+    return join "\n", @rfc822;
+}
+
+sub data2rfc822 {
+    my ($data, $fieldimps) = @_;
+    my $rfc822_str = '';
+
+# based on /usr/lib/dpkg/controllib.pl
+    for my $f (sort { $fieldimps->{$b} <=> $fieldimps->{$a} } keys %$data) {
+	my $v= $data->{$f};
+	$v =~ m/\S/o || next; # delete whitespace-only fields
+	$v =~ m/\n\S/o && warn("field $f has newline then non whitespace >$v<");
+	$v =~ m/\n[ \t]*\n/o && warn("field $f has blank lines >$v<");
+	$v =~ m/\n$/o && warn("field $f has trailing newline >$v<");
+	$v =~ s/\$\{\}/\$/go;
+	$rfc822_str .= "$f: $v\n";
+    }
+
+    return $rfc822_str;
+}
+
+sub get_dpkg_changes {
+    my $changes = "\n $_[0]->{Header}\n .\n$_[0]->{Changes}";
+    chomp $changes;
+    $changes =~ s/^ $/ ./mgo;
+    return $changes;
+}
+
+1;
+__END__
+=head1 SEE ALSO
+
+Parse::DebianChangelog
+
+=head1 AUTHOR
+
+Frank Lichtenheld, E<lt>frank@lichtenheld.deE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2005 by Frank Lichtenheld
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+=cut
