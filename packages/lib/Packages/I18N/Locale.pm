2dc0e356a13c10bed0a7b46140cbb195ffe028f7
Frank Lichtenheld <djpig@debian.org>
2005-10-18 10:52:02 +0000

    Change all ISO locales to UTF-8. This was only necessary on woody since we didn't had them there.

--- packages/lib/Packages/I18N/Locale.pm
+++ packages/lib/Packages/I18N/Locale.pm
@@ -15,23 +15,9 @@ my %lang2loc = ( en => "en_US",
 		 default => "en_US",
 		 );
 
-# most of them can probably changed to UTF-8 in Sarge
-# as there are more available UTF-8 locales then
 my %lang2charset = (
 		    default => 'UTF-8',
-		    cs => 'ISO-8859-2',
-		    da => 'ISO-8859-1',
-		    es => 'ISO-8859-1',
-		    fi => 'ISO-8859-1',
-		    hu => 'ISO-8859-2',
-		    it => 'ISO-8859-1',
 		    ja => 'EUC-JP',
-		    nl => 'ISO-8859-1',
-		    pl => 'ISO-8859-2',
-		    pt_BR => 'ISO-8859-1',
-		    pt_PT => 'ISO-8859-1',
-		    sk => 'ISO-8859-2',
-		    sv_SE => 'ISO-8859-1',
 		    uk => 'KOI8-U',
 		    );
 
