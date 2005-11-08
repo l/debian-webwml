d775370937cb989492988f1a3a1603782e48b9d8
Frank Lichtenheld <djpig@debian.org>
2005-11-08 17:23:19 +0000

    bts2ldap moved again

--- webwml/english/devel/wnpp/wnpp.pl
+++ webwml/english/devel/wnpp/wnpp.pl
@@ -14,7 +14,7 @@ use HTML::Entities;
 # this is ok this way.  It says which server to query, on which port and what
 # to fetch from it.  The attribs array could be reduced.
 
-my $server = "master.debian.org";
+my $server = "bts2ldap.debian.net";
 my $port = "10101";
 my $base   = "dc=current,dc=bugs,dc=debian,dc=org";
 my $attrs  = [
