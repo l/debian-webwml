16215ede3649411ade2ca107ff82a0fdd0ca774c
Jens Seidel <tux-master@web.de>
2006-08-22 10:47:02 +0000

    Extented script to check for proper package versions in DSAs.

--- webwml/english/security/check-used-data.sh
+++ webwml/english/security/check-used-data.sh
@@ -4,7 +4,11 @@
 # current DSA. dsa-1000.wml should contain:
 #include "$(ENGLISHDIR)/security/2006/dsa-1000.data"
 #
+# It also checks that the package versions in DSA match across
+# all translations.
+#
 # It outputs all wrong files. There is no output if all files are OK.
+# Japanese files are sometime false positives (because of a word search).
 #
 # Jens Seidel (jensseidel@users.sf.net), (c) 2006, GPL
 
@@ -15,6 +19,7 @@ if ! test -e english/security; then
   exit 1
 fi
 
+# search for proper inclusion
 for dir in $(find -name "security" -type d); do
   cd $dir
   for dsa in $(find -name "dsa-[0-9]*.wml" -o -name "[0-9]*.wml"); do
@@ -23,5 +28,39 @@ for dir in $(find -name "security" -type d); do
     (echo -n "$dir/$dsa: "; grep "#include" $dsa)
    done
    cd - > /dev/null
+done
+
+# search for proper version numbers
+rm -f /tmp/dsa
+for dsa in $(find english/security -name "dsa-[0-9]*.wml"); do
+  # append all lines to one to simplify search
+  cat $dsa | awk '{file=file " " $0} END {print file }' | sed 's/  * / /g' > .dsa
+  # extract the version string after $phrase
+  for phrase in ' been fixed in version ' \
+                ' will be fixed in version ' \
+		' is fixed in version ' \
+		' are fixed in version ' \
+		' was fixed in version ' \
+		' in version ' \
+		' fixed in upstream version '
+#		' fixed in the upstream version ' # "of Linux "
+  do
+    while grep "$phrase" .dsa > /dev/null; do
+      # matches the last version string (line limit of sed exceeds)
+      #version=$(cat .dsa | sed -n "s/^.*$phrase\([^,< ]*\).*$/\1/p" | sed 's/\.$//')
+      version=$(cat .dsa | perl -p -e "s/^.*$phrase([^,< ]*).*$/\1/" | sed 's/\.$//')
+      version_regex=$(echo $version | sed 's/\./\\./g')
+      # removed the last found match
+      #cat .dsa | sed "s/^\(.*\)$phrase\([^,< ]*\)\(.*\)$/\1\3/" > .dsa.tmp; mv .dsa.tmp .dsa
+      perl -p -i -e "s/^(.*)$phrase([^,< ]*)(.*)$/\1\3/" .dsa
+      for dsa_tr in */$(echo $dsa | sed 's,^english/,,'); do
+        if ! grep -w "$version_regex" $dsa_tr > /dev/null; then
+          echo "Error: version $version does not occur in $dsa_tr"
+        fi
+      done
+    done
+  done
+  # record ignored version strings (to be analyzed later)
+  cat .dsa >> /tmp/dsa
 done
 
