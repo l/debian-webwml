d34b225fbad34fe40c0d06460a3f9c253aadacd5
Frank Lichtenheld <djpig@debian.org>
2006-12-22 10:23:14 +0000

    distribution names can contain '-', fix regex

--- packages/cgi-bin/search_packages.pl
+++ packages/cgi-bin/search_packages.pl
@@ -43,7 +43,7 @@ my $input = new CGI;
 # exit;
 
 my %params_def = ( keywords => { default => undef, match => '^\s*([-+\@\w\/.:]+)\s*$' },
-		   version => { default => 'edgy', match => '^(\w+)$',
+		   version => { default => 'edgy', match => '^([\w-]+)$',
 				replace => { all => '*' } },
 		   case => { default => 'insensitive', match => '^(\w+)$' },
 		   subword => { default => 0, match => '^(\w+)$' },
