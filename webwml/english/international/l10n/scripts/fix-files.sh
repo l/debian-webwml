b79b2c41096f497f66b2a9d9e53a6c897fe9fc0e
Simon Paillard <simon.paillard@resel.enst-bretagne.fr>
2008-08-25 20:07:30 +0000

    Revert previous commit as the patch needs some extra data

--- webwml/english/international/l10n/scripts/fix-files.sh
+++ webwml/english/international/l10n/scripts/fix-files.sh
@@ -10,20 +10,6 @@ for code
 do
     for dist in main contrib non-free
     do
-        echo 'DELETE-ME' > $dir/${dist}-$code.todo-nostatus
-        echo 'DELETE-ME' > $dir/${dist}-$code.todo-status
-        echo 'DELETE-ME' > $dir/${dist}-$code.todo-both
-        if [ -f $dir/${dist}-$code.todo ]
-        then
-            rm -f $dir/${dist}-$code.todo-both
-            if [ -f $dir/../../data/status.$code ]
-            then
-                rm -f $dir/${dist}-$code.todo-status
-            else
-                rm -f $dir/${dist}-$code.todo-nostatus
-            fi
-        fi
-
         for type in todo ok exc
         do
             file=$dir/${dist}-$code.$type
@@ -36,18 +22,9 @@ do
     sed -e "s/@tmpl_lang@/$code/" \
         -e "s/@tmpl_lang_stats@/$stat/" \
         -e "s/href=\"tmpl\\./href=\"$code./" \
-        -e "/LINE: todo-main /r     $dir/main-$code.todo" \
-        -e "/LINE: todo-main-nostatus/r $dir/main-$code.todo-nostatus" \
-        -e "/LINE: todo-main-status/r   $dir/main-$code.todo-status" \
-        -e "/LINE: todo-main-both/r     $dir/main-$code.todo-both" \
-        -e "/LINE: todo-contrib /r  $dir/contrib-$code.todo" \
-        -e "/LINE: todo-contrib-nostatus/r  $dir/contrib-$code.todo-nostatus" \
-        -e "/LINE: todo-contrib-status/r    $dir/contrib-$code.todo-status" \
-        -e "/LINE: todo-contrib-both/r      $dir/contrib-$code.todo-both" \
-        -e "/LINE: todo-non-free /r $dir/non-free-$code.todo" \
-        -e "/LINE: todo-non-free-nostatus/r $dir/non-free-$code.todo-nostatus" \
-        -e "/LINE: todo-non-free-status/r   $dir/non-free-$code.todo-status" \
-        -e "/LINE: todo-non-free-both/r     $dir/non-free-$code.todo-both" \
+        -e "/LINE: todo-main/r     $dir/main-$code.todo" \
+        -e "/LINE: todo-contrib/r  $dir/contrib-$code.todo" \
+        -e "/LINE: todo-non-free/r $dir/non-free-$code.todo" \
         -e "/LINE: ok-main/r     $dir/main-$code.ok" \
         -e "/LINE: ok-contrib/r  $dir/contrib-$code.ok" \
         -e "/LINE: ok-non-free/r $dir/non-free-$code.ok" \
@@ -56,32 +33,7 @@ do
         -e "/LINE: exc-non-free/r $dir/non-free-$code.exc" \
             tmpl.$lang.tmpl |\
         sed -e '/<!-- DO NOT REMOVE THIS LINE/d' |\
-        perl -e '
-my @print = ();
-my @block = ();
-my $print = 1;
-my $block = "";
-while (<>) {
-	if (m/BEGIN SECTION/){
-		$block .= $_;
-		push @print, $print;
-		push @block, $block;
-		$block = "";
-		$print = 1;
-	} elsif (m/END SECTION/) {
-		my $tmp = "";
-		$tmp = $block if $print;
-		$print = pop @print;
-		$block = pop @block;
-		$block .= $tmp;
-		$block .= $_;
-	} elsif (m/DELETE-ME/) {
-		$print = 0;
-	} else {
-		$block .= $_;
-	}
-}
-print $block;' \
+        perl -e 'while(<>) {if (m/BEGIN SECTION/) {$body=$_; $found=0;do {$_ = <>; $found=1 if m/DELETE-ME/; $body.=$_} until m/END SECTION/; print $body unless $found} else {print}}' \
                 > $code.$lang.html
 done
 
