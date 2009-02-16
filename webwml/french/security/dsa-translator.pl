24fe1e61a477dddb66ea44ace59edbdc30d128c5
Thomas Péteul <olaf@resel.fr>
2009-02-16 08:08:34 +0000

    Script revision: * Handle DWWW_LANG and DWWW_MAINT * Add squeeze "translation" * Changed $translation->{String}{LANG} to $translation->{LANG}{String} * Catch more For the ... distribution ... type

--- webwml/french/security/dsa-translator.pl
+++ webwml/french/security/dsa-translator.pl
@@ -1,117 +1,80 @@
-#!env perl -w
+#!/usr/bin/perl -w
 
-# This script translate common parts of DSA
+# This script translate common part of DSA
 
-# Written by Jean-Edouard Babin
+# Written by Jean-Edouard Babin, radius in gmail.com
 
+# Todo
+# - Make a regexp that is enough generic to catch lines like that to be able to use $translation for theses lines instead of s//// '<p>For the <distrib_release> distribution (<distrib_name>) this problem has been fixed in version <version>.</p>' => 
+# - Split translation of "For the ... distribution" and "this problem" in 2 part ?? Not yet sure that it's a good idea even if it would reduce number of s///
+# - Take DSA from URL (http://cvs.debian.org/*checkout*/webwml/english/security/<YEAR>/dsa-<ID>.wml)
+#   or from local copy. use -Y for year and -n for dsa numner, assume -Y as current year if not provided.
+#   Check for ../../english/security/<YEAR>/dsa-<ID>.wml if not found, download it
+# - Save translated DSA instead of print to STDOUT. As to be saved in <YEAR>/dsa-<ID>.wml
+
+# History
+# Revision 1.1 16/02/2009 03:23
+# - Handle DWWW_LANG and DWWW_MAINT
+# - Add squeeze "translation"
+# - Changed $translation->{String}{LANG} to $translation->{LANG}{String}
+#   so LANG alias can be done easily (I mean alias like FR for french)
+# - Catch more "For the ... distribution ..." type
 
 use Getopt::Std;
 #use utf8;
 #binmode(STDOUT, ":utf8");	
 
-# You may put your name here to add it automatically in the translation header
-my $maintainer = ""; 
+# Export your name in DWWW_MAINT or put it here explicitly
+my $maintainer = $ENV{DWWW_MAINT} || "";
 
 my $output= "";
+$opt_l = $ENV{DWWW_LANG} if (exists $ENV{DWWW_LANG});
 getopts('l:f:');
-die "Usage: $0 -f <file> -l <LANG> > <translated_file>" if (!defined($opt_f) or !defined($opt_l));
+die "Usage: $0 -f <file> -l <LANG> > <translted_file>" if (!defined($opt_f) or !defined($opt_l));
 
 
 $translation = {
-		'interpretation conflict'	=> {
-			'FR' => 'conflit d\'interprétation'
-		},
-		'incorrect API usage'	=> {
-			'FR' => 'Mauvaise utilisation de l\'interface de programmation'
-		},
-		'integer overflow'	=> {
-			'FR' => 'Débordement d\'entier'
-		},
-		'insecure temp file generation' => {
-			'FR' => 'Fichiers temporaires peu sûrs'
-		},
-		'insufficient input sanitising' => {
-			'FR' => 'Vérification d\'entrée manquante'
-		},
-		'missing input validation' => {
-			'FR' => 'Validations des entrées insuffisantes'
-		},
-		'several vulnerabilities' => {
-			'FR' => 'Plusieurs vulnérabilités'
-		},
-		'buffer overflow' => {
-			'FR' => 'Débordement de mémoire tampon'
-		},
-		'buffer overflows' => {
-			'FR' => 'Débordements de mémoire tampons'
-		},
-		'programming error' => {
-			'FR' => 'Erreur de programmation'
-		},
-		'heap overflow' => {
-			'FR' => 'Débordement de zone de mémoire du système'
-		},
-		'authorization bypass' => {
-			'FR' => 'Contournement d\'autorisation'
-		},
-		'insufficient input validation' => {
-			'FR' => 'Validations des entrées insuffisantes'
-		},
-		'cross-site scripting' => {
-			'FR' => 'Script intersite'
-		},
-		'design flaw' => {
-			'FR' => 'Défaut de conception'
-		},
-		'design flaws' => {
-			'FR' => 'Défauts de conception'
-		},
-		'denial of service' => {
-			'FR' => 'Déni de service'
-		},
-		'denial of service/privilege escalation' => {
-			'FR' => 'Déni de service et augmentation de droits'
-		},
-		'SQL injection' => {
-			'FR' => 'Injection SQL'
-		},
-		'<p>We recommend that you upgrade your <package> package.</p>' => {
-			'FR' => '<p>Nous vous recommandons de mettre à jour votre paquet <package>.</p>'
-		},
-		'<p>We recommend that you upgrade your <package> packages.</p>' => {
-			'FR' => '<p>Nous vous recommandons de mettre à jour vos paquets <package>.</p>'
-		},
-		'<p>For the <distrib_release> distribution (<distrib_name>) this problem has been fixed in version <version>.</p>' => {
-			'FR' => '<p>Pour la distribution <distrib_release> (<distrib_name>), ce problème a été corrigé dans lav ersion <version>.</p>'
-		},
-		'<p>For the <distrib_release> distribution (<distrib_name>), these problems have been fixed in version <version>.</p>' => {
-			'FR' => '<p>Pour la distribution <distrib_release> (<distrib_name>), ces problèmes ont été corrigés dans la version <version>.</p>'
-		},
-		'etch' => {
-			'FR' => 'Etch'
-		},
-		'sid' => {
-			'FR' => 'Sid'
-		},
-		'lenny' => {
-			'FR' => 'Lenny'
-		}
+	'french'	=> {
+		'interpretation conflict'		=>	'conflit d\'interprétation',
+		'incorrect API usage'			=>	'Mauvaise utilisation de l\'interface de programmation',
+		'integer overflow'				=>	'Débordement d\'entier',
+		'insecure temp file generation'	=>	'Fichiers temporaires peu sûrs',
+		'insufficient input sanitising' =>	'Vérification d\'entrée manquante',
+		'missing input validation'		=>	'Validations des entrées insuffisantes',
+		'several vulnerabilities'		=>	'Plusieurs vulnérabilités',
+		'buffer overflow'				=>	'Débordement de mémoire tampon',
+		'buffer overflows'				=>	'Débordements de mémoire tampons',
+		'programming error'				=>	'Erreur de programmation',
+		'heap overflow'					=>	'Débordement de zone de mémoire du système',
+		'authorization bypass'			=>	'Contournement d\'autorisation',
+		'insufficient input validation'	=>	'Validations des entrées insuffisantes',
+		'cross-site scripting'			=>	'Script intersite',
+		'design flaw'					=>	'Défaut de conception',
+		'design flaws'					=>	'Défauts de conception',
+		'denial of service'				=>	'Déni de service',
+		'SQL injection'					=>	'Injection SQL',
+		'denial of service/privilege escalation'	=>	'Déni de service et augmentation de droits',
+		'<p>We recommend that you upgrade your <package> package.</p>'	=>	'<p>Nous vous recommandons de mettre à jour votre paquet <package>.</p>',
+		'<p>We recommend that you upgrade your <package> packages.</p>'	=>	'<p>Nous vous recommandons de mettre à jour vos paquets <package>.</p>',
+	},
 };
 
+# Lang Alias
+$translation->{'FR'} = $translation->{'french'};
 
 open(FILE, $opt_f) or die "Can't open file $opt_f : $!";
 
 $output = "#use wml::debian::translation-check translation=\"\" maintainer=\"$maintainer\"\n";
-print STDERR "---------------------------------------------------\n";
-print STDERR "Don't forget to complete translation-check header !\n";
-print STDERR "---------------------------------------------------\n";
+print STDERR "---------------------------------------------------\n" if ($maintainer eq "");
+print STDERR "Don't forget to complete translation-check header !\n" if ($maintainer eq "");
+print STDERR "---------------------------------------------------\n" if ($maintainer eq "");
 
 while(<FILE>) {
 	if (m|^<define-tag description>(.+?)</define-tag>$|) {
 		$desc = $1;
-		if (defined($translation->{$desc}{$opt_l})) {
+		if (defined($translation->{$opt_l}{$desc})) {
 			$line = $_;
-			$line =~ s/$desc/$translation->{$desc}{$opt_l}/;
+			$line =~ s/$desc/$translation->{$opt_l}{$desc}/;
 			$output .= $line;
 		} else {
 			print STDERR "I was not able to find a translation for this line !\n";
@@ -121,8 +84,8 @@ while(<FILE>) {
 	}
 	elsif (m|^<p>We recommend that you upgrade your (.+?) package\.</p>$|) {
 		$package_name = $1;
-		if (defined($translation->{'<p>We recommend that you upgrade your <package> package.</p>'}{$opt_l})) {
-			$line = $translation->{'<p>We recommend that you upgrade your <package> package.</p>'}{$opt_l};
+		if (defined($translation->{$opt_l}{'<p>We recommend that you upgrade your <package> package.</p>'})) {
+			$line = $translation->{$opt_l}{'<p>We recommend that you upgrade your <package> package.</p>'};
 			$line =~ s/<package>/$package_name/;
 			$output .= "$line\n";
 		} else {
@@ -133,8 +96,8 @@ while(<FILE>) {
 	}
 	elsif (m|^<p>We recommend that you upgrade your (.+?) packages\.</p>$|) {
 		$package_name = $1;
-		if (defined($translation->{'<p>We recommend that you upgrade your <package> packages.</p>'}{$opt_l})) {
-			$line = $translation->{'<p>We recommend that you upgrade your <package> packages.</p>'}{$opt_l};
+		if (defined($translation->{$opt_l}{'<p>We recommend that you upgrade your <package> packages.</p>'})) {
+			$line = $translation->{$opt_l}{'<p>We recommend that you upgrade your <package> packages.</p>'};
 			$line =~ s/<package>/$package_name/;
 			$output .= "$line\n";
 		} else {
@@ -149,28 +112,31 @@ while(<FILE>) {
 }
 close(FILE);
 
-if ($opt_l eq "FR") {
+if (($opt_l eq "french") || ($opt_l eq "FR")) {
 	# Translation of fixed version messages
-	#$output =~ s|<p>For.the.\S+.distribution.\S+.this.problem.has.been.fixed.in.version.\S+\.</p>|$translation->{'<p>For the <distrib_release> distribution (<distrib_name>) this problem has been fixed in version <version>.</p>'}{$opt_l}|gs;
-	$output =~ s|<p>[\n]{0,1}For.the.(\S+).distribution.\((\S+)\),{0,1}.this.problem.has.been.fixed.in.version.(\S+)\.[\n]{0,1}</p>|<p>Pour la distribution $1 ($2), ce problème a été corrigé dans la version $3.</p>|gs;
-	$output =~ s|<p>[\n]{0,1}For.the.(\S+).distribution.\((\S+)\),{0,1}.these.problems.have.been.fixed.in.version.(\S+)\.[\n]{0,1}</p>|<p>Pour la distribution $1 ($2), ces problèmes ont été corrigés dans la version $3.</p>|gs;
-	$output =~ s|<p>[\n]{0,1}For.the.(\S+).\((\S+)\).distribution,{0,1}.this.problem.has.been.fixed.in.version.(\S+)\.[\n]{0,1}</p>|<p>Pour la distribution $1 ($2), ce problème a été corrigé dans la version $3.</p>|gs;
-	$output =~ s|<p>[\n]{0,1}For.the.(\S+).\((\S+)\).distribution,{0,1}.these.problems.have.been.fixed.in.version.(\S+)\.[\n]{0,1}</p>|<p>Pour la distribution $1 ($2), ces problèmes ont été corrigés dans la version $3.</p>|gs;
+	#$output =~ s|<p>For.the.\S+.distribution.\S+.this.problem.has.been.fixed.in.version.\S+\.</p>|$translation->{$opt_l}{'<p>For the <distrib_release> distribution (<distrib_name>) this problem has been fixed in version <version>.</p>'}|gs;
+	$output =~ s|<p>[\n]?For.the.(\S+).distribution.\((\S+)\),?.this.problem.has.a?l?s?o?.?been.fixed.in.version.(\S+)\.[\n]?</p>|<p>Pour la distribution $1 ($2), ce problème a été corrigé dans la version $3.</p>|gs;
+	$output =~ s|<p>[\n]?For.the.(\S+).distribution.\((\S+)\),?.these.problems.have.a?l?s?o?.?been.fixed.in.version.(\S+)\.[\n]?</p>|<p>Pour la distribution $1 ($2), ces problèmes ont été corrigés dans la version $3.</p>|gs;
+	$output =~ s|<p>[\n]?For.the.(\S+).\((\S+)\).distribution,?.this.problem.has.a?l?s?o?.?been.fixed.in.version.(\S+)\.[\n]?</p>|<p>Pour la distribution $1 ($2), ce problème a été corrigé dans la version $3.</p>|gs;
+	$output =~ s|<p>[\n]?For.the.(\S+).\((\S+)\).distribution,?.these.problems.have.a?l?s?o?.?been.fixed.in.version.(\S+)\.[\n]?</p>|<p>Pour la distribution $1 ($2), ces problèmes ont été corrigés dans la version $3.</p>|gs;
 
-	$output =~ s|<p>[\n]{0,1}For.the.(\S+).distribution.\((\S+)\).and.the.(\S+).distribution.\((\S+)\),{0,1}.this.problem.has.been.fixed.in.version.(\S+)\.[\n]{0,1}</p>|<p>Pour la distribution $1 ($2) et la distribution $3 ($4), ce problème a été corrigé dans la version $5.</p>|gs;
-	$output =~ s|<p>[\n]{0,1}For.the.(\S+).distribution.\((\S+)\).and.the.(\S+).distribution.\((\S+)\),{0,1}.these.problems.have.been.fixed.in.version.(\S+)\.[\n]{0,1}</p>|<p>Pour la distribution $1 ($2) et la distribution $3 ($4), ces problèmes ont été corrigés dans la version $5.</p>|gs;
-	$output =~ s|<p>[\n]{0,1}For.the.(\S+).\((\S+)\).and.(\S+).\((\S+)\) distributions,{0,1}.this.problem.has.been.fixed.in.version.(\S+)\.[\n]{0,1}</p>|<p>Pour la distribution $1 ($2) et la distribution $3 ($4), ce problème a été corrigé dans la version $5.</p>|gs;
-	$output =~ s|<p>[\n]{0,1}For.the.(\S+).\((\S+)\).and.(\S+).\((\S+)\) distributions,{0,1}.these.problems.have.been.fixed.in.version.(\S+)\.[\n]{0,1}</p>|<p>Pour la distribution $1 ($2) et la distribution $3 ($4), ces problèmes ont été corrigés dans la version $5.</p>|gs;
-	$output =~ s|<p>[\n]{0,1}For.the.upcoming.(\S+).distribution.\((\S+)\).and.the.(\S+).distribution.\((\S+)\),{0,1}.this.problem.has.been.fixed.in.version.(\S+)\.[\n]{0,1}</p>|<p>Pour la distribution $1 à venir ($2) et la distribution $3 ($4), ce problème a été corrigé dans la version $5.</p>|gs;
-	$output =~ s|<p>[\n]{0,1}For.the.upcoming.(\S+).distribution.\((\S+)\).and.the.(\S+).distribution.\((\S+)\),{0,1}.these.problems.have.been.fixed.in.version.(\S+)\.[\n]{0,1}</p>|<p>Pour la distribution $1 à venir ($2) et la distribution $3 ($4), ces problèmes ont été corrigés dans la version $5.</p>|gs;
-	$output =~ s|<p>[\n]{0,1}For the.(\S+).distribution.\((\S+)\).and.the.(\S+).distribution.\((\S+)\),{0,1}, this problem will.be.fixed.soon\.[\n]{0,1}</p>|<p>Pour la distribution $1 ($2) et la distribution $3 ($4), ce problème sera corrigé prochainement.</p>|gs;
-	$output =~ s|<p>[\n]{0,1}For the.(\S+).distribution.\((\S+)\).and.the.(\S+).distribution.\((\S+)\),{0,1}, these problems will.be.fixed.soon\.[\n]{0,1}</p>|<p>Pour la distribution $1 ($2) et la distribution $3 ($4), ces problèmes seront corrigés prochainement.</p>|gs;
-	$output =~ s|<p>[\n]{0,1}The.(\S+).distribution.\((\S+)\).will.be.fixed.soon\.[\n]{0,1}</p>|<p>La distribution $1 ($2) sera corrigé prochainement.</p>|gs;
+	$output =~ s|<p>[\n]?For.the.(\S+).distribution.\((\S+)\).and.t?h?e?.??(\S+).distribution.\((\S+)\),?.this.problem.has.a?l?s?o?.?been.fixed.in.version.(\S+)\.[\n]?</p>|<p>Pour la distribution $1 ($2) et la distribution $3 ($4), ce problème a été corrigé dans la version $5.</p>|gs;
+	$output =~ s|<p>[\n]?For.the.(\S+).distribution.\((\S+)\).and.t?h?e?.??(\S+).distribution.\((\S+)\),?.these.problems.have.a?l?s?o?.?been.fixed.in.version.(\S+)\.[\n]?</p>|<p>Pour la distribution $1 ($2) et la distribution $3 ($4), ces problèmes ont été corrigés dans la version $5.</p>|gs;
+	$output =~ s|<p>[\n]?For.the.(\S+).\((\S+)\).and.(\S+).\((\S+)\) distributions,?.this.problem.has.a?l?s?o?.?been.fixed.in.version.(\S+)\.[\n]?</p>|<p>Pour la distribution $1 ($2) et la distribution $3 ($4), ce problème a été corrigé dans la version $5.</p>|gs;
+	$output =~ s|<p>[\n]?For.the.(\S+).\((\S+)\).and.(\S+).\((\S+)\) distributions,?.these.problems.have.a?l?s?o?.?been.fixed.in.version.(\S+)\.[\n]?</p>|<p>Pour la distribution $1 ($2) et la distribution $3 ($4), ces problèmes ont été corrigés dans la version $5.</p>|gs;
+	$output =~ s|<p>[\n]?For.the.upcoming.(\S+).distribution.\((\S+)\).and.the.(\S+).distribution.\((\S+)\),?.this.problem.has.a?l?s?o?.?been.fixed.in.version.(\S+)\.[\n]?</p>|<p>Pour la distribution $1 à venir ($2) et la distribution $3 ($4), ce problème a été corrigé dans la version $5.</p>|gs;
+	$output =~ s|<p>[\n]?For.the.upcoming.(\S+).distribution.\((\S+)\).and.the.(\S+).distribution.\((\S+)\),?.these.problems.have.a?l?s?o?.?been.fixed.in.version.(\S+)\.[\n]?</p>|<p>Pour la distribution $1 à venir ($2) et la distribution $3 ($4), ces problèmes ont été corrigés dans la version $5.</p>|gs;
+	$output =~ s|<p>[\n]?For.the.(\S+).distribution.\((\S+)\).and.the.(\S+).distribution.\((\S+)\),?, this problem will.be.fixed.soon\.[\n]?</p>|<p>Pour la distribution $1 ($2) et la distribution $3 ($4), ce problème sera corrigé prochainement.</p>|gs;
+	$output =~ s|<p>[\n]?For.the.(\S+).distribution.\((\S+)\).and.the.(\S+).distribution.\((\S+)\),?, these problems will.be.fixed.soon\.[\n]?</p>|<p>Pour la distribution $1 ($2) et la distribution $3 ($4), ces problèmes seront corrigés prochainement.</p>|gs;
+	$output =~ s|<p>[\n]?For the.(\S+).distribution.\((\S+)\),?.this problem.will.be.fixed.soon\.[\n]?</p>|<p>Pour la distribution $1 ($2), ce problème sera corrigé prochainement.</p>|gs;
+	$output =~ s|<p>[\n]?For the.(\S+).distribution.\((\S+)\),?.these problems.will.be.fixed.soon\.[\n]?</p>|<p>Pour la distribution $1 ($2), ces problèmes seront corrigés prochainement.</p>|gs;
+	$output =~ s|<p>[\n]?The.(\S+).distribution.\((\S+)\).will.be.fixed.soon\.[\n]?</p>|<p>La distribution $1 ($2) sera corrigé prochainement.</p>|gs;
 
 	# Case adjustment
 	$output =~ s|\(etch\)|\(Etch\)|g;
 	$output =~ s|\(sid\)|\(Sid\)|g;
 	$output =~ s|\(lenny\)|\(Lenny\)|g;
+	$output =~ s|\(squeeze\)|\(Squeeze\)|g;
 
 	# Short recurrent part of text. Take care ! substitution order might be important
 	$output =~ s|Several.vulnerabilities.have.been.discovered in|Plusieurs vulnérabilités ont été découvertes dans|gs;
