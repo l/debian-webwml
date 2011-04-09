502300bc340ad736ea21e50f78536dc35983c1bc
Kan-Ru Chen <koster@debian.org>
2011-04-09 03:16:50 +0000

    Removed.

--- webwml/chinese/bin/toutf8.pl
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/perl -pi
-s|^(\s*<meta http-equiv=.*charset)=big5">|$1=utf-8">|i;
-s|^(\s*<meta http-equiv=.*charset)=gb2312">|$1=utf8">|i;
-s|^(\s*<meta http-equiv=.*charset)=big5hkcs">|$1=utf8">|i;
