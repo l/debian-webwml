6703e0e338e0f5cecfb88a087dd0a5a93fdd879b
Kan-Ru Chen <koster@debian.org>
2011-04-09 16:13:55 +0000

    Put the hand crafted word substitution rules back

--- webwml/chinese/bin/format_zh.pl
+++ webwml/chinese/bin/format_zh.pl
@@ -32,4 +32,22 @@ my @input_array=<>;
 my $c=join("",@input_array);
 $c =~ s/(^[^#].*?)(($cjk_regex)(<a\shref[^>]*>)?)\n((<a\shref[^>]*>)?($cjk_regex))/$1$2$5/mg;
 
+# Convert the Big5 forward slash that's not in the GB2312 code table
+# to the forward slash that's convertible.
+$c =~ s/∕/／/g;
+
+# Note: the following should be automatically generated in the future.
+$c =~ s/<tw支援>/[CN:支援:][HKTW:支持:]/g;
+$c =~ s/<tw(檔|檔案)>/[CN:文件:][HKTW:$1:]/g;
+$c =~ s/<tw文件>/[CN:文檔:][HKTW:文件:]/g;
+$c =~ s/<tw資訊>/[CN:信息:][HKTW:資訊:]/g;
+$c =~ s/<tw連結>/[CN:鏈接:][HKTW:連結:]/g;
+$c =~ s/<tw核心>/[CN:核心:][HKTW:核心:]/g;
+$c =~ s/<tw資料(庫)?>/[CN:數據$1:][HKTW:資料$1:]/g;
+$c =~ s/<tw匯(出|入)>/[CN:導$1:][HKTW:匯$1:]/g;
+$c =~ s/<tw裝置>/[CN:設備:][HKTW:裝置:]/g;
+$c =~ s/<tw連接埠>/[CN:端口:][HKTW:連接埠:]/g;
+$c =~ s/<tw清單>/[CN:列表:][HKTW:清單:]/g;
+$c =~ s/<tw布林>/[CNHK:布爾:][TW:布林:]/g;
+
 print $c;
