f124dd58dc597ec8291fb8746c0e4be3d65b288f
Debian Developer <jfs@debian.org>
2011-10-12 22:38:16 +0000

    Proper copyright and license header

--- webwml/english/security/parse-wml-oval.pl
+++ webwml/english/security/parse-wml-oval.pl
@@ -2,9 +2,27 @@
 # Extracts the data file and creates OVAL queries to
 # be used with the OVAL query interpreter
 # (see http://oval.mitre.org)
+# 
+# Copyright (c) 2004-2005 - Javier Fernandez-Sanguino <jfs@debian.org>
 #
-# (c) 2004 Javier Fernandez-Sanguino
-# Licensed under the GNU General Public License version 2.
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software Foundation:
+#    51 Franklin Street, Suite 500, Boston, MA 02110-1335
+#    (http://www.fsf.org/about/contact/)
+#
+# For more information please see
+#  http://www.gnu.org/licenses/licenses.html#GPL
+#
 
 # Format of data files is:
 #<define-tag pagetitle>DSA-###-# PACKAGE</define-tag>
