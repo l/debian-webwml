2ae567c09c3fe323441bccaf7623e90cafdb786d
David Prévot <taffit@debian.org>
2013-01-26 16:21:43 +0000

    Use tie instead of dbmopen

--- webwml/english/international/l10n/scripts/isoquery.pl
+++ webwml/english/international/l10n/scripts/isoquery.pl
@@ -5,6 +5,7 @@
 
 use strict;
 use warnings;
+use Fcntl; # For O_RDWR, O_CREAT, etc.
 use SDBM_File;
 
 # make the build fail if isoquery is not available
@@ -12,19 +13,19 @@ use SDBM_File;
 
 my @languages=`isoquery -i 639-3`;
 my (%languages,%countries);
-dbmopen(%languages,'../languages',0644) or die($!);
+tie(%languages,'SDBM_File','../languages',O_RDWR|O_CREAT,0644) or die($!);
 foreach ( @languages ) {
 	m/^(\w{3})\t\w\t\w\t(\w{2})?\t(\w{3})?\t(.*)$/; 
 	if (defined $2)    { $languages{$2} = $4; }
 	elsif (defined $3) { $languages{$3} = $4; }
 	elsif (defined $1) { $languages{$1} = $4; }
 }
-dbmclose(%languages) or die($!);
+untie(%languages) or die($!);
 
 my @countries=`isoquery -c`;
-dbmopen(%countries,'../countries',0644) or die($!);
+tie(%countries,'SDBM_File','../countries',O_RDWR|O_CREAT,0644) or die($!);
 foreach ( @countries ) {
 	m/^(\w{2})\t\w{3}\t\d{3}\t(.*)$/;
 	if (defined $1)	{ $countries{$1} = $2; }
 }
-dbmclose(%countries) or die($!);
+untie(%countries) or die($!);
