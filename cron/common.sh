28feacb6c7ba13bc04b55805e7659b4817d3be59
David Prévot <taffit@debian.org>
2013-04-04 16:23:49 +0000

    State that it moved to Git

--- cron/common.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# This file contains some common definitions for the cron scripts
-
-webtopdir=/srv/www.debian.org
-crondir=$webtopdir/cron
-webdir=$webtopdir/www
-ftpdir=$crondir/ftpfiles
-
