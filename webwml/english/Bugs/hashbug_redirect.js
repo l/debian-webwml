968bc3107c20f66960cd7249f2c19643773aeaeb
Paul Wise <pabs@debian.org>
2014-04-29 11:15:22 +0000

    Switch from http to https for bugs.debian.org at DSA request

--- webwml/english/Bugs/hashbug_redirect.js
+++ webwml/english/Bugs/hashbug_redirect.js
@@ -1,5 +1,5 @@
 // If the page's hash seems to be a bug number redirect to
-// http://bugs.debian.org/<hash>
+// https://bugs.debian.org/<hash>
 if (document.location.hash.match(/^#\d+$/)) {
-    document.location = "http://bugs.debian.org/" + document.location.hash.substring(1);
+    document.location = "https://bugs.debian.org/" + document.location.hash.substring(1);
 }
