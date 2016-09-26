ca9f2295a7f816fe7910137d152a18d64690282f
Marcelo Santana <marcelo@msantana.eng.br>
2016-09-26 13:50:11 +0000

    (pt_BR) Add Paulo's email address

--- webwml/portuguese/international/portuguese/translator.db.pl
+++ webwml/portuguese/international/portuguese/translator.db.pl
@@ -84,6 +84,15 @@ sub init_translators {
 	    file      => 0,
 	    compress  => 'none'
 	    },
+	'Paulo Henrique de Lima Santana' => {
+	    email     => 'phls@softwarelivre.org',
+	    summary   => 3,
+	    logs      => 3,
+	    diff      => 3,
+	    tdiff     => 0,
+	    file      => 0,
+	    compress  => 'none'
+	    },
 
 	# Below are special users, used to handle special cases
 	#     default:      default values
